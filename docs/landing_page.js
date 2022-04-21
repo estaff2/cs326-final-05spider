64ae319755eb053e2ad0bbc14246715d885ce6bc
Ethan Stafford <estafford@umass.edu>
2022-04-20 20:13:54 -0400

    nav bar

--- docs/landing_page.js
+++ docs/landing_page.js
@@ -1,5 +1,23 @@
 
 
-
+const onClick = function() {
+    console.log(this.id);
+    let location = window.location.pathname;
+    let path = location.substring(0, location.lastIndexOf("/"));
+    let directoryName = path.substring(path.lastIndexOf("/")+1);
+    console.log(directoryName)
+    let href_string = "";
+    if( directoryName !== 'docs'){
+        href_string = "docs/";
+    }
+    href_string = href_string + this.id + ".html";
+    console.log(href_string);
+    window.location.href = href_string;
+}
+  document.getElementById("landing_page").addEventListener('click', onClick);
+  document.getElementById("user_workout_record_page").addEventListener('click', onClick);
+  document.getElementById("user_workout_history").addEventListener('click', onClick);
+  document.getElementById("user_rec_input").addEventListener('click', onClick);
+  document.getElementById("edit_profile").addEventListener('click', onClick) 
 
 
