4e73bce7534285f38dbb82e99948e6288f1ec5b2
Jack Crowley <Jpcrowley@umass.edu>
2022-05-07 21:53:44 -0400

    log out cleaned out

--- docs/pages/delete_user/delete_user.js
+++ docs/pages/delete_user/delete_user.js
@@ -25,37 +25,7 @@ async function serverRequest(){
 }
 
 document.getElementById("logout").addEventListener('click', async function () {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    await serverRequest();
-    let match1; 
-    let match2;
-    let count = 0;  
-    while (count < users.length){ 
-        match1 = false; 
-        match2 = false; 
-        console.log(users[count]['username']); 
-        console.log(username); 
-        if (users[count]['username'] === username){
-            match1 = true; 
-        }
-        console.log(users[count]['password']); 
-        console.log(password); 
-        if (users[count]['password'] === password){
-            match2 = true; 
-        }
-        if (match1 && match2){
-            window.localStorage.removeItem("user");
-            console.log("successfully logged out");
-            window.location.href = "../landing_page/landing_page.html"; 
-            break;
-        }
-        count++; 
-    }
-    count++; 
-    console.log(count); 
-    console.log(users.length); 
-    if (count > users.length) {
-        alert("Username or Password is incorrect");
-    }
+    window.localStorage.clear("user"); 
+    window.location.href = "../landing_page/landing_page.html";
+
 });
